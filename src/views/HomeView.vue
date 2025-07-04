86ad6dbec2e4604348b569cea478c5c7c4cd9b1d
Liopas <gradmys@gmail.com>
2025-07-04 13:30:26 +0500

    add

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <main>
+     <RouterLink to="/car">Машины</RouterLink>
+     <RouterLink to="/animal">Звери</RouterLink>
+  </main>
+</template>
+
